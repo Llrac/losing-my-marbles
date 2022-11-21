cc83f248ecb4858a12c3a3f010c4a492c44747a0
Llrac <112984468+Llrac@users.noreply.github.com>
2022-11-21 16:58:29 +0100

    added a sloppy player movement

--- Losing_My_Marbles/Assets/Scripts/Movement.cs
+++ Losing_My_Marbles/Assets/Scripts/Movement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(transform.up.x, transform.up.y, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(45, 0, 0);
+        }
     }
 }
