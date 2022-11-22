e69b81d2d8f586d29a2a7443288690a90afafd7b
Llrac <112984468+Llrac@users.noreply.github.com>
2022-11-22 13:23:18 +0100

    added VERY WIP marble system

--- Losing_My_Marbles/Assets/Scripts/GameManager.cs
+++ Losing_My_Marbles/Assets/Scripts/GameManager.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<Marble> deck = new();
+    public List<Marble> discardPile = new();
+    public Transform[] marbleSlots;
+    public bool[] availableMarbleSlots;
+
+    public TextMeshProUGUI deckSizeText;
+    public TextMeshProUGUI discardPileText;
+
+    private void Update()
     {
-        
+        deckSizeText.text = deck.Count.ToString();
+        discardPileText.text = discardPile.Count.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DrawMarbles()
     {
-        
+        if (deck.Count >= 1)
+        {
+            Marble randomMarble = deck[Random.Range(0, deck.Count)];
+
+            for (int i = 0; i < availableMarbleSlots.Length; i++)
+            {
+                if (availableMarbleSlots[i] == true)
+                {
+                    randomMarble.gameObject.SetActive(true);
+                    randomMarble.handIndex = i;
+
+                    randomMarble.transform.position = marbleSlots[i].position;
+                    randomMarble.hasBeenPlayed = false;
+                    availableMarbleSlots[i] = false;
+                    deck.Remove(randomMarble);
+                    return;
+                }
+            }
+        }
+    }
+
+    public void Shuffle()
+    {
+        if (discardPile.Count >= 1)
+        {
+            foreach (Marble marble in discardPile)
+            {
+                deck.Add(marble);
+            }
+            discardPile.Clear();
+        }
     }
 }
---
+++ Losing_My_Marbles/Assets/Scripts/Marble.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marble : MonoBehaviour
+{
+    public bool hasBeenPlayed;
+
+    public int handIndex;
+
+    GameManager gm;
+
+    private void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    public void OnMouseDown()
+    {
+        if (!hasBeenPlayed)
+        {
+            transform.position += Vector3.up * 5;
+            hasBeenPlayed = true;
+            gm.availableMarbleSlots[handIndex] = true;
+            Invoke("MoveToDiscardPile", 2f);
+        }
+    }
+
+    void MoveToDiscardPile()
+    {
+        gm.discardPile.Add(this);
+        gameObject.SetActive(false);
+    }
+}
