4afb8c11dae45f9eab1e21b9f1f1d775eefb8206
Risalord <max.hej777@gmail.com>
2022-11-25 16:53:02 +0100

    added the grid with basic functionality

---
+++ Losing_My_Marbles/Assets/Max_Tests/GridManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    private const char PLAYER = 'P';
+    private const char WALKABLEGROUND = 'X';
+    private float tileSize = 2f;
+    [SerializeField] private GameObject prefab; // remember that the tiles position has its reference pint in the middle of the pillar. needs offset.
+
+    public char[,] board;
+    void Start()
+    {
+        board = new char[9, 9]
+        {
+            {'X','X','X','X','X','P','X','X','X'},
+            {'X','X','X','X','X','X','X','X','X'},
+            {'X','X','X','X','X','X','X','X','X'},
+            {'P','X','X','X','X','X','X','X','P'},
+            {'X','X','X','X','X','X','X','X','X'},
+            {'X','X','X','X','X','X','X','X','X'},
+            {'X','X','X','X','X','X','X','X','X'},
+            {'X','X','X','X','X','X','X','X','X'},
+            {'X','X','X','X','X','X','X','X','X'} // what happens to a player if a hazard turns on wwhile standing in it, GridLogic?
+        };
+        for(int i = 0; i < board.GetLength(0); i++)
+        {
+            for(int j = 0; j < board.GetLength(1); j++)
+            {
+                switch (board[i, j])
+                {
+                    case WALKABLEGROUND:
+                        GameObject newTile = Instantiate(prefab);
+                        float posX = ((i * tileSize + j * tileSize) / 2) + prefab.transform.position.x; // this is the actual x position of the tile
+                        float posY = ((-i * tileSize + j * tileSize) / 4) + prefab.transform.position.y; // this is the actual y position of the tile
+                        newTile.transform.position = new Vector3(posX, posY, 0);
+                        newTile.transform.parent = gameObject.transform;
+                        break;
+                    default:
+                        break;
+                }
+            }
+           
+        }
+        
+    }
+    public void TryMove(GameObject player, Vector2 requestedTile)
+    {
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                switch (board[i, j])
+                {
+                    case WALKABLEGROUND:
+                        float posX = ((i * tileSize + j * tileSize) / 2) + prefab.transform.position.x; // this is the actual x position of the tile
+                        float posY = ((-i * tileSize + j * tileSize) / 4) + prefab.transform.position.y; // this is the actual y position of the tile
+                       
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+        }
+    }
+    
+}
