64c16664464ebcaa0f04a91ca117c59d243848fb
Risalord <max.hej777@gmail.com>
2022-11-28 12:38:17 +0100

    prototyping grid

--- Losing_My_Marbles/Assets/Max_Tests/GridManager.cs
+++ Losing_My_Marbles/Assets/Max_Tests/GridManager.cs
@@ -6,6 +6,7 @@ public class GridManager : MonoBehaviour
 {
     private const char PLAYER = 'P';
     private const char WALKABLEGROUND = 'X';
+    private const char ENEMY = 'E';
     private float tileSize = 2f;
     [SerializeField] private GameObject prefab; // remember that the tiles position has its reference pint in the middle of the pillar. needs offset.
 
@@ -14,26 +15,27 @@ public class GridManager : MonoBehaviour
     {
         board = new char[9, 9]
         {
-            {'P','X','X','X','X','P','X','X','X'},
-            {'X','X','X','X','X','X','X','X','X'},
-            {'X','X','X','X','X','X','X','X','X'},
-            {'P','X','X','X','X','X','X','X','P'},
-            {'X','X','X','X','X','X','X','X','X'},
-            {'X','X','X','X','X','X','X','X','X'},
-            {'X','X','X','X','X','X','X','X','X'},
-            {'X','X','X','X','X','X','X','X','X'},
-            {'X','X','X','X','X','X','X','X','X'} // what happens to a player if a hazard turns on wwhile standing in it, GridLogic?
+            {'X','P','X','X','X','X','X','X','P'},
+                {'X','X','X','X','X','X','X','X','X'},
+                    {'X','X','X','X','X','X','X','X','X'},
+                        {'X','X','X','X','X','X','X','X','X'},
+                            {'X','X','X','X','X','X','X','X','X'},
+                                {'X','X','X','X','X','X','X','X','X'},
+                                    {'X','X','X','X','X','X','X','X','X'},
+                                        {'X','X','X','X','X','X','X','X','X'},
+                                            {'X','X','X','X','X','X','X','X','X'}
+            // what happens to a player if a hazard turns on wwhile standing in it, GridLogic?
         };
-        for(int i = 0; i < board.GetLength(0); i++)
+        for(int x = 0; x < board.GetLength(0); x++)
         {
-            for(int j = 0; j < board.GetLength(1); j++)
+            for(int y = 0; y < board.GetLength(1); y++)
             {
-                switch (board[i, j])
+                switch (board[x, y])
                 {
-                    case PLAYER:
+                    case WALKABLEGROUND:
                         GameObject newTile = Instantiate(prefab);
-                        float posX = ((i * tileSize + j * tileSize) / 2) + transform.position.x; // this is the actual x position of the tile
-                        float posY = ((-i * tileSize + j * tileSize) / 4) + transform.position.y; // this is the actual y position of the tile
+                        float posX = ((x * tileSize + y * tileSize) / 2) + transform.position.x; // this is the actual x position of the tile
+                        float posY = ((-x * tileSize + y * tileSize) / 4) + transform.position.y; // this is the actual y position of the tile
                         newTile.transform.position = new Vector3(posX, posY, 0);
                       //  newTile.transform.parent = gameObject.transform;
                         break;
@@ -44,28 +46,49 @@ public class GridManager : MonoBehaviour
            
         }
         
-    }
-    public void TryMove(GameObject player, Vector2 requestedTile)
-    {
-        Vector2 vector2 = (Vector2)player.transform.position + requestedTile;
-
-        for (int i = 0; i < board.GetLength(0); i++)
+        switch (IsSquareEmpty(0,0))
         {
-            for (int j = 0; j < board.GetLength(1); j++)
-            {
-                switch (board[i, j])
-                {
-                    case WALKABLEGROUND:
-                        float posX = ((i * tileSize + j * tileSize) / 2) + prefab.transform.position.x; // this is the actual x position of the tile
-                        float posY = ((-i * tileSize + j * tileSize) / 4) + prefab.transform.position.y; // this is the actual y position of the tile
-                       
-                        break;
-                    default:
-                        break;
-                }
-            }
+            case 0: //wall
+                //do wall stuff
+                break;
+            case 1: 
+                //Move
+                break;
+            case 2:
+                // player
+                //Push that player && move to square
+                break;
+            case 3:
+                //Push enemy
+                break;
+        }
+        
+    }
+    private void Move(GameObject character, int x, int y)
+    {
+        int savedX ;
+        int savedY ;
 
+        
+    }
+    public int IsSquareEmpty (int x, int y)
+    {
+        if(x > board.GetLength(0) || x < 0 || y > board.GetLength(0) || y < 0)
+                return 0;
+        switch (board[x, y])
+        {
+            case WALKABLEGROUND:
+                return 1;
+
+            case PLAYER:
+                return 2; // maybe should return something else which could be used in another switch statement
+               
+            case ENEMY:
+                return 3;
+
+            default:
+                return 0;
         }
     }
-    
+     
 }
